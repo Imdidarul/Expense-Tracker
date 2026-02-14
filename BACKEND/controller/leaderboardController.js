dc99ed77bdf9780c75b61c4ed0300570625b9414
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-02-14 20:04:57 +0530

    Optimised by adding totalExpense in backend

--- BACKEND/controller/leaderboardController.js
+++ BACKEND/controller/leaderboardController.js
@@ -2,54 +2,54 @@ const Expense = require("../model/expense")
 const User = require("../model/user")
 const sequelize = require("../utils/dbConnection")
 
-// const getLeaderboard = async (req, res) => {
-//     try {
-
-//         if (!req.user.premium) {
-//             return res.status(403).json({
-//                 success: false,
-//                 message: "Only premium users can access leaderboard"
-//             })
-//         }
-
-//         const users = await User.findAll({
-//             attributes: ["name", "totalExpense"],
-//             order: [["totalExpense", "DESC"]]
-//         })
-
-//         return res.status(200).json(users)
-
-//     } catch (error) {
-//         return res.status(500).json({ message: "Something went wrong" })
-//     }
-// }
-
-
-const getLeaderboard = async (req,res)=>{
+const getLeaderboard = async (req, res) => {
     try {
 
-        if (!req.user.premium){
-            return res.status(403).json({message:"Not auothorized"})
+        if (!req.user.premium) {
+            return res.status(403).json({
+                success: false,
+                message: "Only premium users can access leaderboard"
+            })
         }
 
         const users = await User.findAll({
-            attributes:['id', 'name',[sequelize.fn('SUM',sequelize.col('Expenses.amount')), 'totalExpense']],
-            include:[{
-                model: Expense,
-                attributes: []
-            }],
-            group:['Users.id'],
-            order:[['totalExpense', 'DESC']]
+            attributes: ["name", "totalExpense"],
+            order: [["totalExpense", "DESC"]]
         })
 
-        console.log(users)
+        return res.status(200).json(users)
 
-        res.status(200).json(users)
-        
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
+        return res.status(500).json({ message: "Something went wrong" })
     }
 }
+
+
+// const getLeaderboard = async (req,res)=>{
+//     try {
+
+//         if (!req.user.premium){
+//             return res.status(403).json({message:"Not auothorized"})
+//         }
+
+//         const users = await User.findAll({
+//             attributes:['id', 'name',[sequelize.fn('SUM',sequelize.col('Expenses.amount')), 'totalExpense']],
+//             include:[{
+//                 model: Expense,
+//                 attributes: []
+//             }],
+//             group:['Users.id'],
+//             order:[['totalExpense', 'DESC']]
+//         })
+
+//         console.log(users)
+
+//         res.status(200).json(users)
+        
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json(error)
+//     }
+// }
 
 module.exports = { getLeaderboard }
