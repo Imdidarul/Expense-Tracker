41015a0b36c2a81a3127f2821b222d62e180bdaf
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-02-16 21:01:11 +0530

    Added transaction

--- BACKEND/controller/expenseController.js
+++ BACKEND/controller/expenseController.js
@@ -1,7 +1,9 @@
-const {Expense, User} = require("../model")
+const {Expense, User} = require("../model");
+const sequelize = require("../utils/dbConnection");
 // const {Expense, User} = require("../")
 
 const addExpense = async (req,res)=>{
+    const t = await sequelize.transaction()
     try {
         const {amount,description,category} = req.body;
 
@@ -10,19 +12,20 @@ const addExpense = async (req,res)=>{
             description: description,
             category: category,
             userId: req.user.id
-        });
+        }, {transaction: t})
 
-        const user = await User.findOne({where:{id:req.user.id}})
+            const user = await User.findOne({where:{id:req.user.id},transaction:t})
+            
+            user.totalExpense = user.totalExpense + amount
+            await user.save({transaction:t})
 
-        user.totalExpense = user.totalExpense + amount
-
-        await user.save()
-
-        console.log("Expense added")
-        res.status(201).json(expense)
+            await t.commit()
+            console.log("Expense added")
+            res.status(201).json(expense)
     } catch (error) {
-            console.log(error)
-            res.status(500).send("Unable to add expense")
+        await t.rollback()
+        console.log(error)
+        res.status(500).send("Unable to add expense")
     }
 }
 
@@ -47,43 +50,46 @@ const getExpenses = async (req,res) => {
 
 
 const updateExpense = async (req,res)=>{
+    const t = await sequelize.transaction()
     try {
         const {id} = req.params
         const {amount,description,category}= req.body
 
         const expense = await Expense.findOne({
-            where:{id, userId: req.user.id}
+            where:{id, userId: req.user.id},
+            transaction: t
         })
 
         if (!expense){
+            await t.rollback()
             return res.status(404).send("Expense not found")
         }
         const oldAmount = expense.amount
-
         const difference = amount - oldAmount
-
 
         // const [updatedRows] = 
         await expense.update(
-            {amount,description,category}
+            {amount,description,category},
+            {transaction: t}
         )
+
+        const user = await User.findOne({where:{id:req.user.id},transaction:t})
+
+        user.totalExpense = user.totalExpense + difference
+
+        await user.save({transaction:t})
+
+        await t.commit()
+        console.log("Expense updated")
+        res.status(200).send("Expnse updated")
 
         // if (updatedRows === 0){
         //     res.status(404).send("Expense not found")
         //     return
         // }
-        
-
-        const user = await User.findOne({where:{id:req.user.id}})
-
-        user.totalExpense = user.totalExpense + difference
-
-        await user.save()
-
-
-        console.log("Expense updated")
-        res.status(200).send("Expnse updated")
+    
     } catch (error) {
+        await t.rollback()
         res.status(500).send("Unable to update expense")
     }
 }
@@ -91,33 +97,37 @@ const updateExpense = async (req,res)=>{
 
 
 const deleteExpense = async (req,res)=>{
+    const t = await sequelize.transaction()
     try {
         const {id} = req.params
         const expense = await Expense.findOne({
             where:{
                 id:id,
                 userId: req.user.id
-            }
+            },
+            transaction:t
         })
 
         if(!expense){
-            res.status(404).send("Expense not found")
-            return
+            await t.rollback()
+            return res.status(404).send("Expense not found")
         }
 
         const amount = expense.amount
 
-        await expense.destroy()
+        await expense.destroy({transaction:t})
 
-        const user = await User.findOne({where:{id:req.user.id}})
+        const user = await User.findOne({where:{id:req.user.id},transaction:t})
 
         user.totalExpense = user.totalExpense - amount
-        
-
-        await user.save()
+            
+    
+        await user.save({transaction:t})
+        await t.commit()
         console.log("Expense deleted")
         res.status(200).send("Expense is deleted")
     } catch (error) {
+        await t.rollback()
         res.status(500).send("Expense not deleted")
     }
 }
